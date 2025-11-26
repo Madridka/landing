e6f992ab6ffb30dde7aaa151a7df0ac2b4a02fa8
Сербин Кирилл <serbin_kirill1807@mail.ru>
2025-11-26 17:24:21 +0700

    fix: фикс фото в скрипте

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // --- Логика переключения картин (Живая сетка) ---
 const galleryImages = [
-  "/assets/6.jpg",
-  "/assets/5.jpg",
-  "/assets/4.jpg",
-  "/assets/3.jpg",
-  "/assets/2.jpg",
-  "/assets/1.jpg",
+  "assets/6.jpg",
+  "assets/5.jpg",
+  "assets/4.jpg",
+  "assets/3.jpg",
+  "assets/2.jpg",
+  "assets/1.jpg",
 ];
 
 function shuffleGallery() {
